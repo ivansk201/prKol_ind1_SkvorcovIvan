24a283db857651550ca149afbca6a20c56ce181b
BulatDes <bulatdespizek@gmail.com>
2023-04-19 15:31:48 +0500

    Коллекции в с#

---
+++ prack16console/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+class Program
+{
+    static int EvaluateFormula(string formula)
+    {
+        Stack<int> numStack = new Stack<int>();  
+        Stack<char> operStack = new Stack<char>(); 
+        void Calculate()
+        {
+            char oper = operStack.Pop();
+            if (oper == 'M')
+            {
+                int num2 = numStack.Pop();
+                int num1 = numStack.Pop();
+                numStack.Push(Math.Max(num1, num2));
+            }
+            else if (oper == 'm')
+            {
+                int num2 = numStack.Pop();
+                int num1 = numStack.Pop();
+                numStack.Push(Math.Min(num1, num2));
+            }
+        }
+        foreach (char c in formula)
+        {
+            if (char.IsDigit(c))
+            {
+                numStack.Push(int.Parse(c.ToString()));
+            }
+            else if (c == 'M' || c == 'm')
+            {
+                operStack.Push(c);
+            }
+            else if (c == '(')
+            {
+                operStack.Push(c);
+            }
+            else if (c == ')')
+            {
+                while (operStack.Count > 0 && operStack.Peek() != '(')
+                {
+                    Calculate();
+                }
+                operStack.Pop();
+            }
+            else if (c == '|')
+            {
+                while (operStack.Count > 0 && (operStack.Peek() == 'M' || operStack.Peek() == 'm'))
+                {
+                    Calculate();
+                }
+                operStack.Push(c);
+            }
+        }
+
+        while (operStack.Count > 0)
+        {
+            Calculate();
+        }
+
+        return numStack.Pop();
+
+        
+
+    }
+    static void Main()
+    {
+        string filePath = "formula.txt";  
+        using (StreamReader sr = new StreamReader(filePath))
+        {
+            string formula = sr.ReadToEnd(); 
+            int result = EvaluateFormula(formula); 
+            Console.WriteLine($"Значение формулы {formula} = {result}");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ prack16forms/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace prack16forms
+{
+    public partial class Form1 : Form
+    {
+        private Queue<Sotrud> allEmployees = new Queue<Sotrud>();
+        private Queue<Sotrud> youngEmployees = new Queue<Sotrud>();
+        private Queue<Sotrud> oldEmployees = new Queue<Sotrud>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = File.ReadAllLines(openFileDialog.FileName);
+
+                if (lines.Length == 0)
+                {
+                    MessageBox.Show("Открываемый файл пуст", "Ошибка");
+                    return;
+                }
+                    Queue<string> youngEmployees = new Queue<string>();
+                    Queue<string> otherEmployees = new Queue<string>();
+
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Split(',');
+                        int age = int.Parse(parts[4]);
+                        if (age < 30)
+                        {
+                            youngEmployees.Enqueue(line);
+                        }
+                        else
+                        {
+                            otherEmployees.Enqueue(line);
+                        }
+                    }
+
+                    while (youngEmployees.Any())
+                    {
+                        listBox1.Items.Add(youngEmployees.Dequeue());
+                    }
+
+                    while (otherEmployees.Any())
+                    {
+                        listBox1.Items.Add(otherEmployees.Dequeue());
+                    }
+                }
+            }
+        }
+    
+}
+
+    class Sotrud
+    {
+        public string lastName;
+        public string firstName;
+        public string middleName;
+        public string gender;
+        public int age;
+        public decimal salary;
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} ({3}), {4} лет, Зарплата {5:C}",
+                lastName, firstName, middleName, gender, age, salary);
+        }
+    }
